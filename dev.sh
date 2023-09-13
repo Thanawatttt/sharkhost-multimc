63556e1ac40e0d1e5b22ca92c1487a473ddf2e55
Thanawatttt <89511939+Thanawatttt@users.noreply.github.com>
2023-09-14 00:35:29 +0700

    Update dev.sh
    Update ip

--- dev.sh
+++ dev.sh
@@ -51,7 +51,7 @@ function optimizeJavaServer {
 
 # Check if the node IP is matched.
 IP=$(curl -s https://checkip.amazonaws.com)
-if [ ! "$IP" = 194.233.65.137 ]
+if [ ! "$IP" = 140.99.96.61 ]
 then
     display
     echo "$(tput setaf 1)This node is not authorized to use this Multi-Egg. Reason: Invalid IP."
